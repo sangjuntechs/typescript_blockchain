6ee46b1664d3ce9b5afb9e9b0021b0fff998c18d
sangjuntechs <juntech0421@github.com>
2021-01-20 15:20:24 +0900

    hash calc and function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJs from 'crypto-js'
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string,
+  ): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString()
+
   constructor(
     index: number,
     hash: string,
@@ -19,10 +29,17 @@ class Block {
   }
 }
 
+
+
 const genesisBlock:Block = new Block(0,"ejkejr2r22311","","genesis",20210120)
 
-let blockchain: [Block] = [genesisBlock]
+//blockchain은 Block만 삽입 가능
+let blockchain:Block[] = [genesisBlock]
 
-console.log(blockchain)
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
 
 export {};
