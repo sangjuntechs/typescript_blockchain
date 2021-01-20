1377ddaf1c04bb28851dc8d42d5c740cbe54f61b
sangjuntechs <juntech0421@github.com>
2021-01-20 14:40:04 +0900

    create first block

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock:Block = new Block(0,"ejkejr2r22311","","genesis",20210120)
+
+let blockchain: [Block] = [genesisBlock]
+
+console.log(blockchain)
+
+export {};
