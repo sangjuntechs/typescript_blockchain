32d35421fe91640f966684fc2c010501f26297f7
sangjuntechs <juntech0421@github.com>
2021-01-20 13:49:03 +0900

    check typescript running

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert('hello');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'sangjun', age = 24, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name} you are ${age} and sex is ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-alert('hello');
+const name = 'sangjun',
+      age = 24,
+      gender = 'male'
+
+      const sayHi = (name, age, gender?) => {
+          console.log(`Hello ${name} you are ${age} and sex is ${gender}`)
+      }
+    
+    sayHi(name, age, gender)
+
+export {}
