c280054416623253afcc7688b32d7085e48d6f2d
sangjuntechs <juntech0421@github.com>
2021-01-20 15:32:34 +0900

    create new block automatic and hash

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as CryptoJs from 'crypto-js'
+import * as CryptoJs from "crypto-js";
 
 class Block {
   public index: number;
@@ -11,8 +11,9 @@ class Block {
     index: number,
     previousHash: string,
     timestamp: number,
-    data: string,
-  ): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString()
+    data: string
+  ): string =>
+    CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
@@ -29,17 +30,44 @@ class Block {
   }
 }
 
-
-
-const genesisBlock:Block = new Block(0,"ejkejr2r22311","","genesis",20210120)
+const genesisBlock: Block = new Block(
+  0,
+  "ejkejr2r22311",
+  "",
+  "genesis",
+  20210120
+);
 
 //blockchain은 Block만 삽입 가능
-let blockchain:Block[] = [genesisBlock]
+let blockchain: Block[] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+
+  return newBlock;
+};
+
+console.log(createNewBlock('hello'), createNewBlock('byebye'))
 
 export {};
